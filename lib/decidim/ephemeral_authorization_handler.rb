ee218ebe3834bdbfa82d88edffc304f5abaea19b
stephanie rousset <strabon@club-internet.fr>
2026-04-08 10:41:30 +0200

    feat: delete component config

--- lib/decidim/ephemeral_authorization_handler.rb
+++ lib/decidim/ephemeral_authorization_handler.rb
@@ -3,7 +3,6 @@
 require "decidim/ephemeral_authorization_handler/admin"
 require "decidim/ephemeral_authorization_handler/engine"
 require "decidim/ephemeral_authorization_handler/admin_engine"
-require "decidim/ephemeral_authorization_handler/component"
 
 module Decidim
   # This namespace holds the logic of the `EphemeralAuthorizationHandler` component. This component
