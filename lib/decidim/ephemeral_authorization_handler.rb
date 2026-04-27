69764719e1dc783e05e091ba51cf847225dd1c7a
stephanie rousset <strabon@club-internet.fr>
2026-04-27 17:36:50 +0200

    feat: add and update files in lib

--- lib/decidim/ephemeral_authorization_handler.rb
+++ lib/decidim/ephemeral_authorization_handler.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
-require "decidim/ephemeral_authorization_handler/admin"
-require "decidim/ephemeral_authorization_handler/engine"
-require "decidim/ephemeral_authorization_handler/admin_engine"
+require_relative "ephemeral_authorization_handler/verification"
+require_relative "ephemeral_authorization_handler/version"
 
 module Decidim
   # This namespace holds the logic of the `EphemeralAuthorizationHandler` component. This component
   # allows users to create ephemeral_authorization_handler in a participatory space.
   module EphemeralAuthorizationHandler
+    include ActiveSupport::Configurable
+
+    autoload :PhoneNumberFormatter, "decidim/ephemeral_authorization_handler/phone_number_formatter"
+
+    # Default configuration digits to generate the auth code.
+    config_accessor :auth_code_length do
+      4
+    end
+
+    # The country or countries to be selected in country selection
+    # during sms verification/authentication. The default is being set to nil
+    config_accessor :default_countries do
+      nil
+    end
+
+    config_accessor :code_ttl do
+      5.minutes
+    end
   end
 end
---
+++ lib/decidim/ephemeral_authorization_handler/verification.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative "verification/admin"
+require_relative "verification/admin_engine"
+require_relative "verification/engine"
+require_relative "verification/workflow"
