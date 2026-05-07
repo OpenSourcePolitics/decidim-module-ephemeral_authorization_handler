b00cc1fcb5fd7e808370f0d6f3adb4bfc4ff97ee
moustachu <moustachu@opensourcepolitics.eu>
2026-05-07 10:24:15 +0200

    fix: default workflow loading + removing unused admin engine

--- lib/decidim/ephemeral_authorization_handler/verification.rb
+++ lib/decidim/ephemeral_authorization_handler/verification.rb
@@ -1,6 +1,3 @@
 # frozen_string_literal: true
 
-require_relative "verification/admin"
-require_relative "verification/admin_engine"
 require_relative "verification/engine"
-require_relative "verification/workflow"
